d312d2481ca618e9ce1ab9b4a52f6ca79f980978
Kai Ming <gkaim9@gmail.com>
2024-07-15 22:40:00 +0800

    started on pipex

---
+++ Milestone 2/pipex/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 22:26:57 by kagoh             #+#    #+#             */
+/*   Updated: 2024/07/15 22:26:57 by kagoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#endif
