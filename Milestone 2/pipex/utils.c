f0f6064e95e5057b2ba09e4d837d43d8ca043af7
Kai Ming Goh <kagoh@c2r5s7.42singapore.sg>
2024-07-17 17:16:53 +0800

    modified files

--- Milestone 2/pipex/utils.c
+++ Milestone 2/pipex/utils.c
@@ -1,25 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/15 22:26:57 by kagoh             #+#    #+#             */
-/*   Updated: 2024/07/17 16:50:36 by kagoh            ###   ########.fr       */
+/*   Created: 2024/07/17 17:04:23 by kagoh             #+#    #+#             */
+/*   Updated: 2024/07/17 17:04:32 by kagoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/stat.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <unistd.h>
-
-#endif
