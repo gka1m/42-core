f96546ab7ef8d95ba9e6eb0734029c22448d5112
Kai Ming <gkaim9@gmail.com>
2024-07-18 14:14:59 +0800

    updated utils.c
    Added functions as a start to the pipex assignment

--- Milestone 2/pipex/utils.c
+++ Milestone 2/pipex/utils.c
@@ -12,3 +12,30 @@
 
 #include "pipex.h"
 
+/* setting up of environment */
+
+void error_exit(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+void init_env(const char *file1, const char *file2, int *fd_in.int *fd_out)
+{
+    *fd_in = open(file1, O_RDONLY);
+    if (*fd_in < 0)
+    {
+        perror("Error opening file1");
+        exit(1);
+    }
+    *fd_out = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (*fd_out < 0)
+    {
+        perror("Error in creating file2");
+        exit(1);
+    }
+}
+
+void create_pipe(int *pipe_fd)
+{
+}
