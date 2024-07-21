966f76b8b39cc145b23c680f6cb3a3f1fb7e03a9
Kai Ming <gkaim9@gmail.com>
2024-07-21 16:52:35 +0800

    Update utils.c

--- Milestone 2/pipex/utils.c
+++ Milestone 2/pipex/utils.c
@@ -11,3 +11,23 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+char **parse_command(che *cmd, char **env)
+{
+    char **args;
+
+    args = ft_split(cmd, ' ');
+    if (args == NULL || args[0] == NULL)
+        return (NULL);
+}
+
+char **get_path(char **env)
+{
+    while (*env)
+    {
+        if (ft_strncmp(*env, "PATH", 4) == 0)
+            break;
+        env++;
+    }
+    return (ft_split(*env + 5, ':'));
+}
