0d1f1b2a4de5653523f641be97b175a0a0be55d3
Kai Ming <gkaim9@gmail.com>
2024-07-26 17:32:34 +0800

    updated files
    updated:
    1. utils.c
    2. added function prototypes to header file
    3. edited makefile

--- Milestone 2/pipex/pipex.h
+++ Milestone 2/pipex/pipex.h
@@ -27,5 +27,10 @@ int		ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strdup(const char *string);
 size_t	ft_strlcat(char *dest, const char *src, size_t dest_len);
 char	*ft_strcpy(char *dest, const char *src);
+int     check_input_file(char *file);
+char    *check_cmd(char *cmd);
+char    **extract_var(char **env);
+char    *join_paths(const char *dir, const char *cmd);
+char    *search_paths(char *cmd, char **paths);
 
 #endif
--- Milestone 2/pipex/utils.c
+++ Milestone 2/pipex/utils.c
@@ -12,7 +12,7 @@
 
 #include "pipex.h"
 
-int	check_input_file(char *file)
+int check_input_file(char *file)
 {
 	if (access(file, F_OK) != 0)
 	{
@@ -25,4 +25,59 @@ int	check_input_file(char *file)
 		return (-1);
 	}
 	return (0);
+}
+
+char *check_cmd(char *cmd)
+{
+	if (ft_strncmp(cmd, "./", 2) == 0 && access(cmd, X_OK) == 0)
+		return (ft_strdup(cmd));
+	return (NULL);
+}
+
+char **extract_var(char **env)
+{
+	while (*env)
+	{
+		if (ft_strncmp(*env, "PATH=", 5) == 0)
+			return (ft_split(*env + 6, ':'));
+		env++;
+	}
+	return (NULL);
+}
+
+char *join_paths(const char *dir, const char *cmd)
+{
+	size_t	dir_len;
+	size_t	cmd_len;
+	size_t	full_len;
+	char	*full_path;
+
+	dir_len = ft_strlen(dir);
+	cmd_len = ft_strlen(cmd);
+	full_len = dir_len + cmd_len;
+	full_path = malloc(full_len * sizeof(char));
+	if (full_path == NULL)
+	{
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	ft_strcpy(full_path, dir);
+	ft_strcpy(full_path, "/");
+	ft_strcpy(full_path, cmd);
+	return (full_path);
+}
+
+char *search_paths(char *cmd, char **paths)
+{
+	char	*full_path;
+
+	while (*paths)
+	{
+		full_path = join_paths(*paths, cmd);
+		if (access(full_path, X_OK) == 0)
+			return (full_path);
+		free(full_path);
+		paths++;
+	}
+	return (NULL);
 }
