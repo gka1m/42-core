44d09911affc33d7e0acaa6c1833412d3188e1da
Kai Ming <gkaim9@gmail.com>
2024-07-27 15:23:09 +0800

    added functions
    created function get_path to find the full path of the executable

---
+++ Milestone 2/pipex/utils2.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 14:07:17 by kagoh             #+#    #+#             */
+/*   Updated: 2024/07/27 14:07:17 by kagoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+char    *get_path(char *cmd, char **env)
+{
+    char    *execpath;
+    char    **paths;
+    char    **tmp;
+
+    execpath = check_cmd(cmd);
+    if (execpath)
+        return execpath;
+    paths = extract_var(env);
+    if (!paths)
+        return (NULL);
+    execpath = search_paths(cmd, paths);
+    tmp = paths;
+    while (*tmp)
+    {
+        free(*tmp);
+        tmp++;
+    }
+    free(paths);
+    return (execpath);
+}
+
