4cbef136414f192673693313aa90ab6859e9f8eb
Kai Ming <gkaim9@gmail.com>
2024-07-28 16:21:38 +0800

    further progress in pipex assignment

--- Milestone 2/pipex/utils2.c
+++ Milestone 2/pipex/utils2.c
@@ -44,3 +44,61 @@ void    set_pipes(int *pipe1[2], int *pipe2[2])
     }
 }
 
+void    first_fork(char *cmd, char **env, int in_fd, int *pipe1[2])
+{
+    pid_t   pid;
+    char    *exec_path;
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0)
+    {
+        dup2(in_fd, STDIN_FILENO);
+        dup2((*pipe1)[1], STDOUT_FILENO);
+        close((*pipe1[0]));
+        close((*pipe1[1]));
+        exec_path = get_path(cmd, env);
+        if (exec_path == NULL)
+        {
+            ft_printf("Error: Command  %s not found\n", cmd);
+            exit(EXIT_FAILURE);
+        }
+        execve(exec_path, ft_split(cmd, ' '), env);
+        perror('execve');
+        exit(EXIT_FAILURE);
+    }  
+}
+
+void    second_fork(char *cmd, char **env, int out_fd, int *pipe1[2])
+{
+    pid_t   pid;
+    char    *exec_path;
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0)
+    {
+        dup2(out_fd, STDIN_FILENO);
+        dup2((*pipe1)[1], STDOUT_FILENO);
+        close((*pipe1[0]));
+        close((*pipe1[1]));
+        exec_path = get_path(cmd, env);
+        if (exec_path == NULL)
+        {
+            ft_printf("Error: Command  %s not found\n", cmd);
+            exit(EXIT_FAILURE);
+        }
+        execve(exec_path, ft_split(cmd, ' '), env);
+        perror('execve');
+        exit(EXIT_FAILURE);
+    }  
+}
+
