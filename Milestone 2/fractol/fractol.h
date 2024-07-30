d36f748857d3425792b4e0a6b9aedd238e9f7fb5
Kai Ming <gkaim9@gmail.com>
2024-07-30 15:05:23 +0800

    starting on fractol project

---
+++ Milestone 2/fractol/fractol.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 14:40:23 by kagoh             #+#    #+#             */
+/*   Updated: 2024/07/30 14:40:23 by kagoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+#define FRACTOL_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "mlx.h"
+
+#endif
