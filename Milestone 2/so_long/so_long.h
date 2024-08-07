8433f1b1e300b029c96f8ed99130c85d6dd36b39
Kai Ming Goh <kagoh@c2r5s7.42singapore.sg>
2024-08-07 16:17:05 +0800

    added files for so_long

---
+++ Milestone 2/so_long/so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 14:46:52 by kagoh             #+#    #+#             */
+/*   Updated: 2024/08/07 16:13:12 by kagoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <math.h>
+# include "minilibx/mlx.h"
+# include "minilibx/mlx_int.h"
+
+#endif
