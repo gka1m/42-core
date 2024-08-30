8451f256279ebe18e3aa50b091976f2a625bb067
Kai Ming <gkaim9@gmail.com>
2024-08-30 21:51:18 +0800

    changed the map checking fns
    - changed map checking fns to use ft_split instead of gnl

--- Milestone 2/so_long/check_map.c
+++ Milestone 2/so_long/check_map.c
@@ -12,47 +12,109 @@
 
 #include "so_long.h"
 
-int	get_map_height(t_game *game, const char *map)
+char *read_file(char *mapfile)
 {
-	int		fd;
-	char	*line;
-	int		height;
+	int fd;
+	char buffer[1024];
+	int bytes_read;
+	char *content;
 
-	height = 0;
-	fd = open(map, O_RDONLY);
+	fd = open(mapfile, O_RDONLY);
 	if (fd < 0)
 	{
-		ft_printf("Failed to open map file\n");
+		perror("Error opening file");
 		exit(1);
 	}
-	game->map.height = 0;
-	while ((line = get_next_line(fd)) != NULL)
+	bytes_read = read(fd, buffer, 1024);
+	if (bytes_read == -1)
 	{
-		height++;
-		free(line);
+		perror("Error reading file");
+		exit(1);
 	}
 	close(fd);
-	return (height);
+	content = malloc((bytes_read + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	ft_strlcpy(mapfile, buffer, 1024);
+	mapfile[bytes_read] = '\0';
+	return (mapfile);
 }
 
-void	load_data(t_game *game, const char *map)
+int validate_shape(char **lines)
 {
-	int		fd;
-	char	*line;
-	int		i;
+	int width;
+	int i;
+
+	width = ft_strlen(lines[0]);
+	i = 0;
+	while (lines[i] != NULL)
+	{
+		if (ft_strlen(lines[i]) != width)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void count_elements(char **lines, int *exit, int *collectibles, int *start)
+{
+	int i;
+	int j;
 
 	i = 0;
-	fd = open(map, O_RDONLY);
-	if (fd < 0)
+	j = 0;
+	while (lines[i] != NULL)
 	{
-		ft_printf("Failed to open map file\n");
-		exit(1);
+		while (lines[i][j] != '\0')
+		{
+			if (lines[i][j] == 'E')
+				*exit++;
+			if (lines[i][j] == 'C')
+				*collectibles++;
+			if (lines[i][j] == 'P')
+				*start++;
+			j++;
+		}
+		i++;
 	}
-	game->map.map_array = malloc(game->map.height * sizeof(char));
-	if (game->map.map_array == NULL)
-		return (NULL);
-	while ((line = get_next_line(fd)) != NULL)
-		game->map.map_array[i++] = line;
-	close(fd);
-	game->map.width = ft_strlen(game->map.map_array[0]);
+}
+
+int check_closure(char **lines, int width)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (lines[i] != NULL)
+	{
+		if (lines[i][0] != '1' || lines[i][width - 1] != '1')
+			return (0);
+		i++;
+	}
+	while (j < width)
+	{
+		if (lines[0][j] != '1' || lines[i - 1][j] != '1')
+			return (0);
+		j++;
+	}
+	return (1);
+}
+
+int validate_map(char **lines)
+{
+	int exits;
+	int start;
+	int collectibles;
+	int width;
+
+	width = ft_strlen(lines[0]);
+	if (!validate_shape(lines))
+		return (0);
+	count_elements(lines, &exits, &collectibles, &start);
+	if (exits != 1 || collectibles < 1 || start != 1)
+		return (0);
+	if (!check_closure(lines, width))
+		return (0);
+	return (1);
 }
---
+++ Milestone 2/so_long/ft_split.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 11:13:59 by kagoh             #+#    #+#             */
+/*   Updated: 2024/05/27 11:13:59 by kagoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static size_t count_words(char const *s, char c)
+{
+	size_t wordcount;
+	int i;
+
+	i = 0;
+	wordcount = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			wordcount++;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+	}
+	return (wordcount);
+}
+
+static char *create_word(const char *s, size_t start, size_t end)
+{
+	char *word;
+	size_t i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	if (word == NULL)
+		return (NULL);
+	while (i < end - start)
+	{
+		word[i] = s[start + i];
+		i++;
+	}
+	word[end - start] = '\0';
+	return (word);
+}
+
+void free_word(char **split_arr)
+{
+	int i;
+
+	i = 0;
+	while (split_arr[i] != NULL)
+	{
+		free(split_arr[i]);
+		i++;
+	}
+	free(split_arr);
+}
+
+char **split_string(char const *s, char c, char **result, size_t wordcount)
+{
+	size_t i;
+	size_t j;
+	size_t start;
+	size_t k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (k < wordcount)
+	{
+		while (s[j] != '\0' && s[j] == c)
+			j++;
+		start = j;
+		while (s[j] != '\0' && s[j] != c)
+			j++;
+		result[i] = create_word(s, start, j);
+		if (result[i++] == NULL)
+		{
+			free_word(result);
+			return (NULL);
+		}
+		k++;
+	}
+	result[i] = NULL;
+	return (result);
+}
+
+char **ft_split(char const *s, char c)
+{
+	size_t wordcount;
+	char **result;
+
+	if (s == NULL)
+		return (NULL);
+	wordcount = count_words(s, c);
+	result = malloc((wordcount + 1) * sizeof(char *));
+	if (result == NULL)
+		return (NULL);
+	return (split_string(s, c, result, wordcount));
+}
