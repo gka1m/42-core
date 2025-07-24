c743dba81ee21195f2bcda1bd8e49b85d60b42dd
Kai Ming Goh <kagoh@c1r7s2.42singapore.sg>
2025-07-24 13:45:01 +0800

    finished CPP7

--- Milestone 5/CPP7/ex02/Array.hpp
+++ Milestone 5/CPP7/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 16:19:13 by kagoh             #+#    #+#             */
-/*   Updated: 2025/07/23 16:30:49 by kagoh            ###   ########.fr       */
+/*   Updated: 2025/07/24 13:41:10 by kagoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,30 @@
 #include <string>
 #include <cstddef>
 #include <stdexcept>
+#include <cstdlib>
 
 template <typename T>
 class Array
 {
     private:
         T* data;
-        int size;
+        unsigned int _size;
 
     public:
-        Array() : data(NULL), size(0) {}
-        Array(int n) : data(new T[n]), size(n) {}
-        Array(const Array& other) : data(NULL), size(0) {}
+        Array() : data(NULL), _size(0) {}
+        Array(unsigned int n) : data(new T[n]), _size(n) {}
+        Array(const Array& other) : data(NULL), _size(0) 
+        {
+            *this = other;
+        }
         Array& operator=(const Array& other)
         {
             if (this != &other)
             {
                 delete[] data;
-                size = other.size;
-                data = new T[size];
-                for (int i = 0; i < size; i++)
+                _size = other._size;
+                data = new T[_size];
+                for (unsigned int i = 0; i < _size; i++)
                     data[i] = other.data[i];
             }
             return *this;
@@ -44,6 +48,27 @@ class Array
         ~Array()
         {
             delete[] data;
+        }
+        
+        // subscript operator
+        T& operator[](unsigned int idx)
+        {
+            if (idx >= _size)
+                throw std::out_of_range("Index is out of bounds");
+            return data[idx];
+        }
+        
+        // const subscript operator (overloading with const-qualification)
+        const T& operator[](unsigned int idx) const
+        {
+            if (idx >= _size)
+                throw std::out_of_range("Index is out of bounds");
+            return data[idx];
+        }
+        
+        unsigned int size() const
+        {
+            return _size;
         }
 };
 
--- Milestone 5/CPP7/ex02/main.cpp
+++ Milestone 5/CPP7/ex02/main.cpp
@@ -1,53 +1,112 @@
 #include <iostream>
-#include <Array.hpp>
+#include "Array.hpp"
 
 #define MAX_VAL 750
-int main(int, char**)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
+
+// own main
+int main() {
+    std::cout << "=== Testing Default Constructor ===" << std::endl;
+    Array<int> a;
+    std::cout << "a.size(): " << a.size() << std::endl;
+
+    std::cout << "\n=== Testing Parameterized Constructor ===" << std::endl;
+    Array<int> b(5);
+    for (unsigned int i = 0; i < b.size(); ++i)
+        b[i] = i * 10;
+
+    std::cout << "Contents of b: ";
+    for (unsigned int i = 0; i < b.size(); ++i)
+        std::cout << b[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\n=== Testing Copy Constructor ===" << std::endl;
+    Array<int> c(b);
+    std::cout << "Contents of c (copied from b): ";
+    for (unsigned int i = 0; i < c.size(); ++i)
+        std::cout << c[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\n=== Modifying c to test deep copy ===" << std::endl;
+    c[0] = 999;
+    std::cout << "b[0]: " << b[0] << " (should be unchanged)" << std::endl;
+    std::cout << "c[0]: " << c[0] << std::endl;
+
+    std::cout << "\n=== Testing Assignment Operator ===" << std::endl;
+    Array<int> d;
+    d = b;
+    std::cout << "Contents of d (assigned from b): ";
+    for (unsigned int i = 0; i < d.size(); ++i)
+        std::cout << d[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\n=== Modifying d to test deep copy ===" << std::endl;
+    d[1] = 888;
+    std::cout << "b[1]: " << b[1] << " (should be unchanged)" << std::endl;
+    std::cout << "d[1]: " << d[1] << std::endl;
+
+    std::cout << "\n=== Testing Bounds Checking ===" << std::endl;
+    try {
+        std::cout << "Accessing b[10]: ";
+        std::cout << b[10] << std::endl; // should throw
+    } catch (const std::exception &e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+    try {
+        std::cout << "Accessing b[-1]: ";
+        std::cout << b[-1] << std::endl; // should throw
+    } catch (const std::exception &e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
     return 0;
 }
